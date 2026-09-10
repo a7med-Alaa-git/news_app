cc780f533d9503b8c4daf90d8f0194fa98d75e07
ahmed3ab3al <ahmedalaaabdalalw9@gmail.com>
2026-09-10 15:52:42 +0300

    article model

---
+++ lib/models/article_model.dart
@@ -0,0 +1,15 @@
+class ArticleModel {
+  final String title;
+  final String image;
+  final String date;
+
+  ArticleModel({required this.title, required this.image, required this.date});
+
+  factory ArticleModel.fromjson(dynamic json) {
+    return ArticleModel(
+      title: json['title'],
+      image: json['urlToImage'],
+      date: json['publishedAt'],
+    );
+  }
+}

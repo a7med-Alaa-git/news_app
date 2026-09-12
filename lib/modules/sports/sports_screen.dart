2419bf6de968194c37d812db412656bf7d0b27a9
a7med-Alaa-git <ahmedalaaabdalalw9@gmail.com>
2026-09-12 09:40:44 +0300

    sports  data in design

--- lib/modules/sports/sports_screen.dart
+++ lib/modules/sports/sports_screen.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/cubit/cubit.dart';
+import 'package:news_app/widgets/articles_list_view.dart';
 
 class SportsScreen extends StatelessWidget {
   const SportsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ArticlesListView(
+      articleList: AppCubit.get(context).sports,
+      image:
+          'https://student-cms.prd.timeshighereducation.com/sites/default/files/styles/default/public/different_sports.jpg?itok=CW5zK9vp',
+    );
   }
 }

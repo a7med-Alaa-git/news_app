01a67d1df74fb2e57b84ef10cfd256464da33615
a7med-Alaa-git <ahmedalaaabdalalw9@gmail.com>
2026-09-12 09:33:22 +0300

    appear science data in design

--- lib/modules/science/science_screen.dart
+++ lib/modules/science/science_screen.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/cubit/cubit.dart';
+import 'package:news_app/cubit/states.dart';
+import 'package:news_app/widgets/articles_list_view.dart';
 
 class ScienceScreen extends StatelessWidget {
   const ScienceScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text('Science ');
+    return BlocConsumer<AppCubit, AppStates>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        return ArticlesListView(
+          articleList: AppCubit.get(context).science,
+          image:
+              'https://img.magnific.com/free-vector/hand-drawn-science-education-background_23-2148499325.jpg?semt=ais_hybrid&w=740&q=80',
+        );
+      },
+    );
   }
 }
